120299a1d040855614f4a9593d733804d2a4d3eb
ekmstudios <errol@ekmstudios.com>
2019-07-13 13:56:55 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<?php
+$txt = "Hello World";
+
+?>
+
+<h1><?php echo $txt;?></h1>
+<p>Sample Hello world page</p>
+
+</body>
+</html>
